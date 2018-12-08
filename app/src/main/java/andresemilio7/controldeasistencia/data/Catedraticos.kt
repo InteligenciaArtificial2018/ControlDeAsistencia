6510f2105af9dbe5fa746a136e414a11c8ffeb83
Andres Aguirre <emilio_acampos@hotmail.com>
2018-12-08 17:10:20 -0600

    Tabla Catedraticos

--- app/src/main/java/andresemilio7/controldeasistencia/data/Catedraticos.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/data/Catedraticos.kt
@@ -1,4 +1,21 @@
 package andresemilio7.controldeasistencia.data
 
-class Catedraticos {
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "Catedraticos")
+class Catedraticos(
+    @ColumnInfo(name = "Nombre")
+    var nombre: String = "",
+    @ColumnInfo(name = "Apellido")
+    var apellido: String = "",
+    @ColumnInfo(name = "Correo")
+    var correo: String = "",
+    @ColumnInfo(name = "Facultad")
+    var facultad: String = "",
+    @ColumnInfo(name = "Telefono")
+    var telefono: Int = 0) {
+    @PrimaryKey(autoGenerate = false)
+    var id: String = ""
 }
