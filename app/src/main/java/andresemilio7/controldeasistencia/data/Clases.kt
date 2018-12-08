bdba04a9e634e054fb0cc064c0bf4a39326a659c
Andres Aguirre <emilio_acampos@hotmail.com>
2018-12-08 17:20:04 -0600

    Tabla Clases

--- app/src/main/java/andresemilio7/controldeasistencia/data/Clases.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/data/Clases.kt
@@ -1,4 +1,17 @@
 package andresemilio7.controldeasistencia.data
 
-class Clases {
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "Clases")
+class Clases(
+    @ColumnInfo(name = "Asignatura")
+    var asignatura: String = "",
+    @ColumnInfo(name = "Secciòn")
+    var seccion: String = "",
+    @ColumnInfo(name = "Aula")
+    var aula: Int = 0) {
+    @PrimaryKey(autoGenerate = false)
+    var idCodigo: String = ""
 }
