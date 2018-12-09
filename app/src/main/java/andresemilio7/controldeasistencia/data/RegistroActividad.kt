1b666e6789c857d3fa485193f7633c7b45979c0f
Andres Aguirre <emilio_acampos@hotmail.com>
2018-12-08 18:58:32 -0600

    Tabla Registro de Actividad/Foreign Key

--- app/src/main/java/andresemilio7/controldeasistencia/data/RegistroActividad.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/data/RegistroActividad.kt
@@ -1,4 +1,28 @@
 package andresemilio7.controldeasistencia.data
 
-class RegistroActividad {
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
+
+@Entity (tableName = "Registo de Actividad",
+    foreignKeys = arrayOf(ForeignKey(
+    entity = Clases::class,
+    parentColumns = arrayOf("idCodigo"),
+    childColumns = arrayOf("idcodigo")
+)))
+
+class RegistroActividad(
+    @ColumnInfo(name = "Hora")
+    var hora: String = "",
+    @ColumnInfo(name = "Fecha")
+    var fecha: String = "",
+    @ColumnInfo(name = "Codigo de Clase")
+    var idcodigo: String = "",
+    @ColumnInfo(name = "Revision")
+    var revision: Boolean ) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+
+
 }
