1d542ca61efb232ea998335e6aafd96d4e0a18dd
Andres Aguirre <emilio_acampos@hotmail.com>
2018-12-09 23:20:20 -0600

    Cambios en Clases/Hora

--- app/src/main/java/andresemilio7/controldeasistencia/data/Clases.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/data/Clases.kt
@@ -4,12 +4,15 @@ import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
+
 @Entity(tableName = "Clases")
 class Clases(
     @ColumnInfo(name = "Asignatura")
     var asignatura: String = "",
     @ColumnInfo(name = "Secciòn")
     var seccion: String = "",
+    @ColumnInfo(name = "Hora")
+    var hora: String ="",
     @ColumnInfo(name = "Aula")
     var aula: Int = 0) {
     @PrimaryKey(autoGenerate = false)
