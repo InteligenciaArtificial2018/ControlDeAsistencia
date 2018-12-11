a3c3dadcc385ed5ba60743cc54442b5afb42def4
Andres Aguirre <emilio_acampos@hotmail.com>
2018-12-11 03:38:16 -0600

    Cambios Multiples!

--- app/src/main/java/andresemilio7/controldeasistencia/MainActivity.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/MainActivity.kt
@@ -1,12 +1,36 @@
 package andresemilio7.controldeasistencia
 
+import andresemilio7.controldeasistencia.data.Clases
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btncat = findViewById<Button>(R.id.btnAgregarCatedraticoMain)
+        val btnclase = findViewById<Button>(R.id.btnAgregarClaseMain)
+        val btnrev = findViewById<Button>(R.id.btnRegistroactividadMain)
+
+        btncat.setOnClickListener {
+            val intent = Intent(this, AgregarCatedratico::class.java)
+            startActivity(intent)
+        }
+
+        btnclase.setOnClickListener {
+            val intent = Intent(this, AgregarClase::class.java)
+            startActivity(intent)
+        }
+
+        btnrev.setOnClickListener {
+            val intent = Intent(this,RegistroActividad::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
--- app/src/main/java/andresemilio7/controldeasistencia/RegistroActividad.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/RegistroActividad.kt
@@ -1,4 +1,12 @@
 package andresemilio7.controldeasistencia
 
-class RegistroActividad {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RegistroActividad : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registroactividad)
+    }
 }
