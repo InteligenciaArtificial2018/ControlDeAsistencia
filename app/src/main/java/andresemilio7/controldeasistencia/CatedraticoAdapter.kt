ac71fcda7be8210bbbe8f80bc8f45dcadf1a6a8e
Andres Aguirre <emilio_acampos@hotmail.com>
2018-12-11 18:42:36 -0600

    Crecion de catedratico adapter

---
+++ app/src/main/java/andresemilio7/controldeasistencia/CatedraticoAdapter.kt
@@ -0,0 +1,5 @@
+package andresemilio7.controldeasistencia
+
+class CatedraticoAdapter {(var catedraticosList: List<Catedraticos>? = ArrayList<Catedrativos>()): RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+    private var onCatedraticosItemClickListener = onCatedraticosItemClickListener? == null
+}
