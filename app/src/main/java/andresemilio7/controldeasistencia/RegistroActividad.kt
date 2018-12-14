947aeac27664ec0130b6866fecc90e899d74ae49
Andres Aguirre <emilio_acampos@hotmail.com>
2018-12-14 03:11:22 -0600

    Clase Agregar Registro

--- app/src/main/java/andresemilio7/controldeasistencia/RegistroActividad.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/RegistroActividad.kt
@@ -1,12 +1,68 @@
 package andresemilio7.controldeasistencia
 
+import andresemilio7.controldeasistencia.data.ControlDeAsistenciaDatabase
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.RadioGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.agregarcatedratico.*
+import kotlinx.android.synthetic.main.template_registro.*
 
 class RegistroActividad : AppCompatActivity() {
+    private var controlAsistenciaDB: ControlDeAsistenciaDatabase? = null
+    private var revision = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registroactividad)
+
+
+        controlAsistenciaDB = ControlDeAsistenciaDatabase.getInstance(this)
+        rgRevision.setOnCheckedChangeListener(this)
+
+        val asignatura = intent.getStringExtra("idcodigo")
+        val catedratico = intent.getStringExtra("idCatedratico")
+        val hora = intent.getStringExtra("hora")
+        val aula = intent.getStringExtra("aula")
+        val fecha = intent.getStringExtra("fecha")
+
+
+        if (asignatura == null  || asignatura == ""){
+            btnAgregarRevision.setOnClickListener {
+                val revision = RegistroActividad(etAsignatura.text.toString(), etCatedratico.text.toString(), etHora.text.toString(), etAula.text.toString(), etFechaRev.text.toString(), revision)
+                controlAsistenciaDB?.getRegistroActividadDAO()?.saveRegistroActividad(revision)
+                Toast.makeText(this, "Guardado", Toast.LENGTH_SHORT).show()
+                finish()
+
+            }
+        } else{
+            etAsignatura.setText(asignatura)
+            etCatedratico.setText(catedratico)
+            etHora.setText(hora)
+            etAula.setText(aula)
+            etFechaRev.setText(fecha)
+            btnAgregarRevision.setOnClickListener{
+                val revision = RegistroActividad(etAsignatura.text.toString(), etCatedratico.text.toString(), etHora.text.toString(), etAula.text.toString(), etFechaRev.text.toString(), revision)
+                controlAsistenciaDB?.getRegistroActividadDAO()?.updateRegistroActividad(revision)
+                Toast.makeText(this, "No guardado", Toast.LENGTH_SHORT).show()
+                finish()
+
+            }
+
+
+        }
+
+
     }
+
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        if (checkedId == R.id.rbVerificado) {
+            prioridad = 1
+        } else if (checkedId == R.id.rbNoRealizado) {
+            prioridad = 2
+        }
+    }
+
+
+
 }
--- app/src/main/java/andresemilio7/controldeasistencia/data/RegistroActividad.kt
+++ app/src/main/java/andresemilio7/controldeasistencia/data/RegistroActividad.kt
@@ -5,20 +5,22 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
 
-@Entity (tableName = "Registo de Actividad"
+@Entity (tableName = "RegistoDeActividad"
 )
 
 class RegistroActividad(
-    @ColumnInfo(name = "Hora")
-    var hora: String = "",
-    @ColumnInfo(name = "Fecha")
-    var fecha: String = "",
     @ColumnInfo(name = "CodigoClase")
     var idcodigo: String = "",
     @ColumnInfo(name = "Catedratico")
     var idCatedratico: String = "",
+    @ColumnInfo(name = "Hora")
+    var hora: String = "",
+    @ColumnInfo(name = "Aula")
+    var aula: String = "",
+    @ColumnInfo(name = "Fecha")
+    var fecha: String = "",
     @ColumnInfo(name = "Revision")
-    var revision: Boolean ) {
+    var revision: Int = 0 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 
